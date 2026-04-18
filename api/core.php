5b0c53f4dccdbd09f853349ae3641b2eee37e5f5
ahmad0105 <ahmad.hashmi.c@gmail.com>
2026-04-18 04:15:56 +0300

    Fix: Add api proxy folder for Vercel compliance

---
+++ api/core.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Vercel Proxy for Backend API
+ */
+chdir(__DIR__ . '/../algraphybackend/public/');
+require 'index.php';
---
+++ api/main.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Vercel Proxy for Main Site
+ */
+chdir(__DIR__ . '/../');
+require 'index.php';
